b8b5a144f762374ef775890fa9791eb91919c0f4
MP <masjidalikhlasp@gmail.com>
2025-05-19 12:17:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//const SPREADSHEET_ID = "1vOZ-fdAOK8hyylppLDHCudiumUiR8hvZ7OL58IrSt34";
+const SPREADSHEET_ID = "1vOZ-fdAOK8hyylppLDHCudiumUiR8hvZ7OL58IrSt34";
 //const SHEET_NAME = "JadwalPBM";
 
 let schedule = [];
 let lastTriggered = null;
 
 async function fetchSchedule() {
-  const url = `https://opensheet.elk.sh/1vOZ-fdAOK8hyylppLDHCudiumUiR8hvZ7OL58IrSt34/JadwalPBM`;
+  const url = `https://opensheet.elk.sh/${SPREADSHEET_ID}/JadwalPBM`;
   try {
     const res = await fetch(url);
     schedule = await res.json();
